69bed373937373ad24611308fb1c6f8e18f52b6d
Fuentes <matheus.fu.p@hotmail.com>
2022-04-23 14:11:35 -0300

    :sparkles: Adicição de arquivos

---
+++ bifurcacao.py
@@ -0,0 +1,22 @@
+from apres_decisoes import decisao_3
+from bif_escolhas import bi_escolha_1
+
+
+def bifurcacao_1():
+    print('Mesmo desconfiada daquele bar ela decide adentrar no local para conseguir se abrigar do sol quente e tenta alguma informação')
+    print('Dentro daquele local ela começa a conversar com algumas pessoas estranhas tentando informação')
+    print('Vendo que ela não é dá li, eles começam a tentar abusa-lá, mas aparece um rapaz simpatico e defende ela.\nEle pergunta a ela...')
+    print('- O que uma garota como você está fazendo perdida neste local?')
+    print('- Eu fugi de casa para tentar a vida na cidade grande.\n\n ...\n')
+    print('Após horas convesando ele a convence que tem um emprego perfeito para ela, ela como não via maldade nas pessoas então decide segui-lo.')
+    print('durante o caminho ele promete a ela varias e varias coisas...\nMoradia, trabalho, uma vida melhor...')
+    print('Ela acreditando em tudo que ele fala. Cria esperança em melhorar sua vida.\n\n')
+    print('Meses depois...\n')
+    print('Ela está jogada num beco toda suja, desarrumada e bebendo. Pensado:\n Aquele cara que ela conheceu que tinha prometido a ela uma oportunidade de vida melhor,\nna verdade era um Cafetão mentindo e fazendo ela ser convencer que aquilo que ela faria era certo.')
+    print('Ela cansada de está passando por aquilo decide..')
+    decisao_3()
+    bi_escolha_1()
+
+
+def bifurcacao_2():
+    return True
